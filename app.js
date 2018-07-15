6c64ae70601a197445552ab8dbdf36d0908fec7c
Michael G Muther <mgmuther@gmail.com>
2018-07-15 10:48:41 +0000

    Adds two file input and addition

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 var express = require('express')
-    , app = express();
+    , app = express()
+    , convertExcel = require('excel-as-json').processFile;
     
+var file1 = './excel_files/balance2.xlsx';
+var file2 = './excel_files/balance3.xlsx';
+var number1;
+var number2;
+var number3;
+    
+// convertExcel('./excel_files/excelTest2.xlsx', null, {isColOriented: true}, function(err,data){
+convertExcel(file1, null, null, function(err,data){
+    if(err){
+        console.log(err);
+    } else {
+        // console.log(data);
+        number1 = data[0]['Balance'];
+        // console.log(number1);
+        convertExcel(file2, null, null, function(err,data){
+            if(err){
+                console.log(err);
+            } else {
+                // console.log(data);
+                number2 = data[0]['Balance'];
+                // console.log(number2);   
+                number3 = number1 + number2;
+                console.log(number3);
+            }
+        });
+    }
+});
+
 // REQUIRING ROUTES
 var indexRoutes = require('./routes/index');
 
